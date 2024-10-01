8f2d553a18e22b07de76038d3935cf7ccc225eec
MagicBag-tab <est24347@uvg.edu.gt>
2024-10-01 07:39:57 -0600

    Para leer los datos de las serpientes

--- LeerCSV.java
+++ LeerCSV.java
@@ -4,4 +4,27 @@ public class LeerCSV {
     public LeerCSV(String archivo) {
         this.archivo = archivo;
     }
+
+    public List<Serpientes> Leer() {
+        List<Serpientes> animales = new ArrayList<>();
+        String linea;
+        String separador = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(separador);
+                double longitud = Double.parseDouble(datos[0]);
+                String especie = datos[1];
+                String color_piel = datos[2];
+                boolean tipo_veneno = Integer.parseInt(datos[3]) == 1;
+
+                Serpientes serpiente = new Serpientes(longitud, especie, color_piel, tipo_veneno);
+                animales.add(serpiente);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return animales;
+    }
 }
